6fbe58610bd9ba4933952623dbcfd08bf99177a1
KS-KIM <kwangsoonkim@outlook.com>
2019-02-10 18:20:40 +0900

    Util class 생성

---
+++ app/src/main/java/com/example/giftishare/utils/PermissionUtils.java
@@ -0,0 +1,43 @@
+package com.example.giftishare.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by KS-KIM on 19/02/08.
+ */
+
+public final class PermissionUtils {
+
+    public static final int PERMISSION_STORAGE_TAG = 255;
+
+    private static Dialog sAlertDialog;
+
+    public static boolean canReadStorage(Activity activity) {
+        return ContextCompat.checkSelfPermission(activity,
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean checkReadStoragePermission(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !canReadStorage(activity)) {
+            requestStoragePermission(activity);
+            return false;
+        }
+        return true;
+    }
+
+    private static void requestStoragePermission(Activity activity) {
+        ActivityCompat.requestPermissions(activity,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PERMISSION_STORAGE_TAG);
+    }
+}
