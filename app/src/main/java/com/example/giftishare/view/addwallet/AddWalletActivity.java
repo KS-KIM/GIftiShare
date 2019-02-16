293d7b0128a1bcdef510027fe5ef972e6d5f0ca6
KS-KIM <kwangsoonkim@outlook.com>
2019-02-16 22:11:14 +0900

    web3j 최신버전에서 지갑이 생성되지 않는 이슈 해결

--- app/src/main/java/com/example/giftishare/view/addwallet/AddWalletActivity.java
+++ app/src/main/java/com/example/giftishare/view/addwallet/AddWalletActivity.java
@@ -18,6 +18,11 @@ import com.example.giftishare.databinding.ActivityAddWalletBinding;
 import com.example.giftishare.utils.PermissionUtils;
 import com.example.giftishare.view.main.MainActivity;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.Provider;
+import java.security.Security;
+
 /**
  * Created by KS-KIM on 19/02/08.
  */
@@ -42,6 +47,7 @@ public class AddWalletActivity extends AppCompatActivity {
 
         setupToolbar();
         setupCreateWalletButton();
+        setupBouncyCastle();
     }
 
     private AddWalletViewModel obtainViewModel(FragmentActivity activity) {
@@ -76,6 +82,24 @@ public class AddWalletActivity extends AppCompatActivity {
 
         mAddWalletViewModel.getNewWalletEvent().observe(this,
                 (@Nullable Event<Object> event) -> openMainActivity());
+    }
+
+    private void setupBouncyCastle() {
+        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
+        if (provider == null) {
+            // Web3j will set up the provider lazily when it's first used.
+            return;
+        }
+        if (provider.getClass().equals(BouncyCastleProvider.class)) {
+            // BC with same package name, shouldn't happen in real life.
+            return;
+        }
+        // Android registers its own BC provider. As it might be outdated and might not include
+        // all needed ciphers, we substitute it with a known BC bundled in the app.
+        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+        // of that it's possible to have another BC implementation loaded in VM.
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+        Security.insertProviderAt(new BouncyCastleProvider(), 1);
     }
 
     public void openMainActivity() {
