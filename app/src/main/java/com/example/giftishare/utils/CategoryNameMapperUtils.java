6d5ab45f2f3053ea6d47b0dec0ffa95c88e67937
KS-KIM <kwangsoonkim@outlook.com>
2019-02-22 00:09:06 +0900

    DB와 명칭이 일치하지 않는 한글명 -> 영어명 map 수정

--- app/src/main/java/com/example/giftishare/utils/CategoryNameMapperUtils.java
+++ app/src/main/java/com/example/giftishare/utils/CategoryNameMapperUtils.java
@@ -8,14 +8,14 @@ public final class CategoryNameMapperUtils {
     private static Map<String, String> map = new HashMap<String, String>() {
         {
             put("음료", "caffee");
-            put("배달음식", "food");
+            put("배달음식", "delivery_food");
             put("음식점", "restaurant");
             put("편의점", "mart");
             put("뷰티", "beauty");
             put("여행", "trip");
-            put("상품권", "gift");
+            put("상품권", "gift_coupon");
             put("책", "book");
-            put("모바일 데이터", "mobile");
+            put("모바일 데이터", "mobile_data");
             put("영화", "movie");
             put("리빙/가전", "living");
             put("교육", "education");
