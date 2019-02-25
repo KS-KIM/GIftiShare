6bb4ef3349e5e9c412694ff793ab3e3942e737d0
KS-KIM <kwangsoonkim@outlook.com>
2019-02-26 00:08:27 +0900

    메인화면 개선 및 지갑 관련 기능 (송금하기, 잔액확인) 추가

--- app/src/main/java/com/example/giftishare/view/main/MainFragment.java
+++ app/src/main/java/com/example/giftishare/view/main/MainFragment.java
@@ -1,10 +1,16 @@
 package com.example.giftishare.view.main;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.giftishare.databinding.FragmentMainBinding;
 
@@ -24,11 +30,6 @@ public class MainFragment extends Fragment {
         return new MainFragment();
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -39,5 +40,53 @@ public class MainFragment extends Fragment {
         mFragmentMainBinding.setLifecycleOwner(getActivity());
         setHasOptionsMenu(true);
         return mFragmentMainBinding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mFragmentMainBinding.walletAddress.setOnClickListener((view) -> {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, mFragmentMainBinding.walletAddress.getText());
+            Intent chooser = Intent.createChooser(intent, "지갑 주소 공유하기");
+            startActivity(chooser);
+        });
+
+        mMainViewModel.getWalletBalance().observe(this,
+                walletBalance -> mFragmentMainBinding.walletBalance.setText(walletBalance));
+
+        mMainViewModel.getSendEtherCompleteEvent().observe(this,
+                stringEvent -> showMessage(stringEvent.getContentIfNotHandled()));
+
+        mFragmentMainBinding.btnSendEther.setOnClickListener((view) -> {
+            final EditText address = new EditText(getContext());
+            final EditText sendValue = new EditText(getContext());
+            sendValue.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            builder.setTitle("받으실 분의 지갑 주소를 입력하세요")
+                    .setView(address)
+                    .setPositiveButton("확인", (DialogInterface dialog, int which) -> {
+                        builder.setTitle("보내실 금액을 입력하세요")
+                                .setView(sendValue)
+                                .setPositiveButton("확인",
+                                        (DialogInterface ethDialog, int ethWhich) -> mMainViewModel.sendEther(address.getText().toString(), sendValue.getText().toString()))
+                                .setNegativeButton("취소",
+                                        (DialogInterface ethDialog, int ethWhich) -> ethDialog.dismiss())
+                                .show();
+                    })
+                    .setNegativeButton("취소", (DialogInterface dialog, int which) -> dialog.dismiss())
+                    .show();
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMainViewModel.start();
+    }
+
+    public void showMessage(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/giftishare/view/main/MainViewModel.java
+++ app/src/main/java/com/example/giftishare/view/main/MainViewModel.java
@@ -11,13 +11,24 @@ import com.example.giftishare.Event;
 import com.example.giftishare.data.DataManager;
 import com.example.giftishare.data.model.CouponsCategoryType;
 
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+
+import java.math.BigDecimal;
+
 public class MainViewModel extends AndroidViewModel {
 
     private static final String TAG = MainViewModel.class.getSimpleName();
 
+    public final MutableLiveData<String> mWalletAddress = new MutableLiveData<>();
+
+    public final MutableLiveData<String> mWalletBalance = new MutableLiveData<>();
+
     private final DataManager mDataManager;
 
     private final MutableLiveData<Event<CouponsCategoryType>> mOpenOnSaleCouponsEvent = new MutableLiveData<>();
+
+    private final MutableLiveData<Event<String>> mSendEtherCompleteEvent = new MutableLiveData<>();
 
     private final Context mContext;
 
@@ -27,6 +38,25 @@ public class MainViewModel extends AndroidViewModel {
         mDataManager = dataManager;
     }
 
+    public LiveData<String> getWalletBalance() {
+        return mWalletBalance;
+    }
+
+    // @TODO Notification으로 변경 예정
+    public LiveData<Event<String>> getSendEtherCompleteEvent() {
+        return mSendEtherCompleteEvent;
+    }
+
+    /**
+     * @apiNote 지갑 잔액 테스트용. 추후 지갑 관리 액티비티에서 호출
+     */
+    public void start() {
+        String walletAddress = mDataManager.getCredentials().getAddress();
+        mWalletAddress.setValue(walletAddress);
+        mWalletBalance.setValue("잔액 조회중...");
+        getBalance();
+    }
+
     public LiveData<Event<CouponsCategoryType>> getOpenOnSaleCouponsEvent() {
         return mOpenOnSaleCouponsEvent;
     }
@@ -34,5 +64,30 @@ public class MainViewModel extends AndroidViewModel {
     public void openOnSaleCoupons(CouponsCategoryType couponsCategoryType) {
         Log.d(TAG, "open onSale coupons function called with " + couponsCategoryType.toEng());
         mOpenOnSaleCouponsEvent.setValue(new Event<>(couponsCategoryType));
+    }
+
+    public void sendEther(String toAddress, String weiValue) {
+        BigDecimal balance = new BigDecimal(weiValue);
+        mDataManager.sendEther(toAddress, balance).thenAccept(TransactionReceipt -> {
+            Log.i(TAG, "send complete. blockHash: " + TransactionReceipt.getBlockHash());
+            mSendEtherCompleteEvent.postValue(new Event<>("송금을 완료했습니다."));
+            getBalance();
+        }).exceptionally(TransactionReceipt -> {
+            Log.i(TAG, "send failed. Detail Message: " + TransactionReceipt.getMessage());
+            mSendEtherCompleteEvent.postValue(new Event<>("송금에 실패했습니다. 다시 시도해주세요."));
+            return null;
+        });
+    }
+
+    public void getBalance() {
+        mDataManager.getBalance().thenAccept(balance -> {
+            String walletBalance = balance.getBalance().toString();
+            Log.i(TAG, "wallet balance loaded: " + walletBalance);
+            mWalletBalance.postValue(walletBalance + " wei");
+        }).exceptionally(transactionReceipt -> {
+            Log.d(TAG, "failed to load balance. network not available" + transactionReceipt.getMessage());
+            mWalletBalance.postValue("잔액을 불러올 수 없습니다. 네트워크 상태를 확인하세요.");
+            return null;
+        });
     }
 }
